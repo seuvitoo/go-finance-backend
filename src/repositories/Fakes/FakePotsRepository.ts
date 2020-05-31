b3c9f85967f86db3c733a16346e9717c87806d09
Victor dos Santos <vy.santos@live.com>
2020-05-30 21:18:57 -0300

    [feature/Goals] add test goals

--- src/repositories/Fakes/FakePotsRepository.ts
+++ src/repositories/Fakes/FakePotsRepository.ts
@@ -1,6 +1,9 @@
+import { uuid } from 'uuidv4';
+
 import Pots from '../../models/Pots';
 
 interface PotsDTO {
+  id: string;
   title: string;
   value: number;
 }
@@ -9,11 +12,29 @@ class PotsRepository {
   private pots: Pots[] = [];
 
   public async create({ title, value }: PotsDTO): Promise<PotsDTO> {
-    pots = new Pots();
-    this.pots.value = value;
-    this.pots.title = title;
+    const pots = new Pots();
 
+    Object.assign(pots, { id: uuid(), value, title });
+
+    this.pots.push(pots);
+    return pots;
+  }
+
+  public async findPots(): Promise<Pots[]> {
     return this.pots;
+  }
+
+  public async findEspecifcPots({ id }: PotsDTO): Promise<Pots | undefined> {
+    const findPots = this.pots.find(pot => pot.id === id);
+    return findPots;
+  }
+
+  public async updatePots({ id, title, value }: PotsDTO): Promise<Pots> {
+    const findPot = this.pots.find(pot => pot.id === id);
+
+    const updatedPot = Object.assign(findPot, { id, title, value });
+
+    return updatedPot;
   }
 }
 
